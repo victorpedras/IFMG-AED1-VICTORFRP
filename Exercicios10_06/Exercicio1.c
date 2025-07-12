27034ad8072d35f61a5906b3ba05b3588b113f55
Victor Fernandes Ribeiro Pedras <victorfrpedras@gmail.com>
2025-07-12 16:45:12 -0300

    Exercicio 1 do dia 10_06

---
+++ Exercicios10_06/Exercicio1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main() {
+    int vetor1[5], vetor2[5], vetor1_2[5], i;
+
+    //Leitura e calculo dos vetores
+    for(i = 0; i < 5; i++) {
+
+        //Leitura vetor1
+        printf("Digite o valor da posicao %d do primeiro vetor: ", i);
+        scanf(" %d", &vetor1[i]);
+        
+        //Leitura vetor2
+        printf("Digite o valor da posicao %d do segundo vetor: ", i);
+        scanf(" %d", &vetor2[i]);
+
+        //Calculo vetor1_2
+        vetor1_2[i] = (vetor1[i] - vetor2[i]);
+    }
+    puts("----------------------------------------------------------");
+
+    //Mostrando vetores 
+    printf("-------Vetor 1-------\n");
+    for(i = 0; i < 5; i++) {
+        printf("| %d ", vetor1[i]);
+    }
+    puts("|");
+
+    puts("");
+    printf("-------Vetor 2-------\n");
+    for(i = 0; i < 5; i++) {
+        printf("| %d ", vetor2[i]);
+    }
+    puts("|");
+
+    puts("");
+    printf("-----Vetor 1 - 2-----\n");
+    for(i = 0; i < 5; i++) {
+        printf("| %d ", vetor1_2[i]);
+    }
+    puts("|");
+    return 0;
+}
